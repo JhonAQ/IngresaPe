a79e8fede712588b155c1ed3016267d397dc2736
Gocardi <diegoalejandrocarbajal@gmail.com>
2025-12-29 11:52:25 -0500

    feat(auth): integrate JWT authentication and add jsonwebtoken dependency

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TrpcService } from './trpc.service';
 import { AppRouter } from './app.router';
-import { PrismaService } from './prisma.service'; // <--- 1. Importar
+import { PrismaService } from './prisma.service';
+import { AuthRouter } from './routers/auth.router';
 
 @Module({
-  imports: [],
+  imports: [
+    // Configuración Global de JWT
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'secret',
+      signOptions: { expiresIn: '7d' }, // El token dura 7 días
+    }),
+  ],
   controllers: [],
   providers: [
-    TrpcService, 
-    AppRouter, 
-    PrismaService // <--- 2. Agregar a la lista de providers
+    TrpcService,
+    PrismaService,
+    AppRouter,
+    AuthRouter,
   ],
 })
 export class AppModule {}
